63cd99ed66e2e5af9a9a77c2c1edd0c7d9d40760
jackkitte <jackkitte735@gmail.com>
2020-04-15 11:58:59 +0900

    カウントアップ/ダウンアプリの永続化

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import MemoStore from './memo/Store';
+
+let state_value = {
+  counter: 0,
+  message: "COUNTER"
+}
+
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+        message: 'INCREMENT'
+      };
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1,
+        message: 'DECREMENT'
+      };
+    case 'RESET':
+      return {
+        counter: 0,
+        message: 'RESET'
+      };
+    default:
+      return state;
+  }
+}
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, counter)
+
+let store = createStore(persistedReducer)
+let pastore = persistStore(store)
 
 ReactDOM.render(
-  <Provider store={MemoStore}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pastore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
